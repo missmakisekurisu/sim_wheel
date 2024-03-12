abd616491375c8fa8aadef15682f76c134b47934
missmakisekurisu <missmakisekurisu@outlook.com>
2024-03-12 14:45:05 +0800

    change function: buttonZ use interupt

--- encoder.c
+++ encoder.c
@@ -2,11 +2,71 @@
 
 int currentRad = 0;
 
+static void Key1_NVIC_Configuration(void)
+{
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+
+    NVIC_InitStructure.NVIC_IRQChannel = KEY1_INT_EXTI_IRQ;
+    
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+static void EXTI_Key1_Config(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    EXTI_InitTypeDef EXTI_InitStructure;
+
+    /*开启按键GPIO口的时钟*/
+    RCC_APB2PeriphClockCmd(KEY1_INT_GPIO_CLK,ENABLE);
+
+    /* 配置 NVIC 中断*/
+    Key1_NVIC_Configuration();
+
+    /*--------------------------KEY1配置---------------------*/
+    /* 选择按键用到的GPIO */
+    GPIO_InitStructure.GPIO_Pin = KEY1_INT_GPIO_PIN;
+    /* 配置为浮空输入 */
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(KEY1_INT_GPIO_PORT, &GPIO_InitStructure);
+
+    /* 选择EXTI的信号源 */
+    GPIO_EXTILineConfig(KEY1_INT_EXTI_PORTSOURCE, \
+                        KEY1_INT_EXTI_PINSOURCE);
+    EXTI_InitStructure.EXTI_Line = KEY1_INT_EXTI_LINE;
+
+    /* EXTI为中断模式 */
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    /* 上升沿中断 */
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+    /* 使能中断 */
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+}
+
+void KEY1_IRQHandler(void)
+{
+    //确保是否产生了EXTI Line中断
+    if (EXTI_GetITStatus(KEY1_INT_EXTI_LINE) != RESET) {
+        // 
+        currentRad = 0U;
+        //清除中断标志位
+        EXTI_ClearITPendingBit(KEY1_INT_EXTI_LINE);
+    }
+}
+
 void encoder_gpio_init(void){
-    //enable clock
+    EXTI_Key1_Config();
     ENCODER_PORT_APBxClock_FUN(ENCODER_PORT_CLK_SRC, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure = {
-        .GPIO_Pin = ENCODER_PIN_A |ENCODER_PIN_B| ENCODER_PIN_Z,
+        .GPIO_Pin = ENCODER_PIN_A |ENCODER_PIN_B,
         .GPIO_Speed = GPIO_Speed_50MHz,
         .GPIO_Mode = GPIO_Mode_IN_FLOATING,
     };
@@ -16,7 +76,7 @@ void encoder_gpio_init(void){
 
     
 uint8_t encoder_read_position(void){
-    if(!ENCODER_READ_Z){currentRad = 0;}
+    //if(!ENCODER_READ_Z){currentRad = 0;}
     static uint8_t state = 0;
     static uint8_t lastState = 0;
 //    if(currentRad > ENCODER_RESOLUTION || currentRad <(-ENCODER_RESOLUTION)){
--- encoder.h
+++ encoder.h
@@ -1,17 +1,31 @@
 #ifndef _ENCODER_H
 #define _ENCODER_H
 #include "stm32f10x.h"
+#include "stm32f10x_exti.h"
 
+//编码器中间按钮用中断
+#define KEY1_INT_GPIO_PORT         GPIOB
+#define KEY1_INT_GPIO_CLK          (RCC_APB2Periph_GPIOB\
+                                    |RCC_APB2Periph_AFIO)
+#define KEY1_INT_GPIO_PIN          GPIO_Pin_9
+#define KEY1_INT_EXTI_PORTSOURCE   GPIO_PortSourceGPIOB
+#define KEY1_INT_EXTI_PINSOURCE    GPIO_PinSource9
+#define KEY1_INT_EXTI_LINE         EXTI_Line9
+#define KEY1_INT_EXTI_IRQ          EXTI9_5_IRQn
+
+#define KEY1_IRQHandler            EXTI9_5_IRQHandler
+
+
+//编码器AB相用轮询
 #define ENCODER_PORT                 GPIOB     
 #define ENCODER_PIN_A                GPIO_Pin_7 
 #define ENCODER_PIN_B                GPIO_Pin_8 
-#define ENCODER_PIN_Z                GPIO_Pin_9 
 #define ENCODER_PORT_CLK_SRC         RCC_APB2Periph_GPIOB
 #define ENCODER_PORT_APBxClock_FUN   RCC_APB2PeriphClockCmd
 
 #define ENCODER_READ_A  GPIO_ReadInputDataBit(ENCODER_PORT, ENCODER_PIN_A)
 #define ENCODER_READ_B  GPIO_ReadInputDataBit(ENCODER_PORT, ENCODER_PIN_B)
-#define ENCODER_READ_Z  GPIO_ReadInputDataBit(ENCODER_PORT, ENCODER_PIN_Z )
+//#define ENCODER_READ_Z  GPIO_ReadInputDataBit(ENCODER_PORT, ENCODER_PIN_Z )
 
 #define ENCODER_RESOLUTION  (19)
 
